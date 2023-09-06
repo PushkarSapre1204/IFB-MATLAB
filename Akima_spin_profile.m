9cce815e0ee7365349634dc5021266f489aec5c0
PushkarSapre1204 <86977783+PushkarSapre1204@users.noreply.github.com>
2023-09-06 10:53:06 +0530

    Data from testing
    Various plots obtained from testing are stored for future reference

--- Akima_spin_profile.m
+++ Akima_spin_profile.m
@@ -1,5 +1,5 @@
 function rpm = Akima_spin_profile(spin_profile)    
-    t = 1:0.5:200;
+    t = 1:0.5:1130;
     rpm = interp1(spin_profile(1,:), spin_profile(2,:), t, "pchip");
     plot(t, rpm)
     hold on
