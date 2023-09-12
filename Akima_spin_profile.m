bd43b822199f6499f4a2325c96da864ef791d868
PushkarSapre1204 <86977783+PushkarSapre1204@users.noreply.github.com>
2023-09-12 17:00:02 +0530

    Added own plot for akima profile

--- Akima_spin_profile.m
+++ Akima_spin_profile.m
@@ -1,6 +1,7 @@
 function rpm = Akima_spin_profile(spin_profile)    
-    t = 1:0.5:1130;
-    rpm = interp1(spin_profile(1,:), spin_profile(2,:), t, "pchip");
+    t = 1:0.5:spin_profile(1, end);
+    figure("Name", "Spin profile", NumberTitle="off")
+    rpm = interp1(spin_profile(1,:), spin_profile(2,:), t, "linear");
     plot(t, rpm)
     hold on
     plot(spin_profile(1,:), spin_profile(2,:), '-o')
