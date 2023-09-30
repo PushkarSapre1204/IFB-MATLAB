35cfd4a575e73fc43d6780092ebe5e70c87dc427
PushkarSapre1204 <86977783+PushkarSapre1204@users.noreply.github.com>
2023-09-30 09:17:48 +0530

    Code cleanup

--- ODE_test.m
+++ ODE_test.m
@@ -1,32 +1,14 @@
-k = 18;
-m = 40;
-c = 240 ;
-F0 = 0.5;
-tmax_rpm = 250; 
-omega_max = 1400*2*pi/60;
-%omega_profile = [0, 1];
-simDuration = [0, 400];
-y0 = [0, 0];
-itr = 0;
-
-% Create figure for ive RPM plotting
-Live_RPM = figure("Name", "Live_RPM", 'NumberTitle','off');
-figure(Live_RPM)
-RPM_line = animatedline;
+k = 1;
+m = 1;
+c = 0.01 ;
+F0 = 1;
+omega_rate = 10^-5;
+simDuration = [0, 4*10^5];
+y0 = [0,0,0.6];
 
 % Solve the system. Store the amplitude and timesteps in a vector
-[t,y] = ode45(@(t,y) SHM(t, y, m, k, c, F0, omega_max, tmax_rpm), simDuration, y0);
+[t,y] = ode45(@(t,y) SHM(t, y, m, k, c, F0, omega_rate), simDuration, y0);
 
-%% For post plotting of RPM
-% This is used to find if the RPM is ramping up correctly. It is believed
-% that for some reason the RPM is not ramping to max RPM in the linear section
-
-time = linspace(0, simDuration(2) , simDuration(2)*10);         %Create additional time vector
-out = zeros([simDuration(2)*10, 1]);                            %Calculate and store the output in a new vector
-
-for i  = 1:simDuration(2)*10
-    out(i) = omega_F(i/10, omega_max, tmax_rpm);
-end
 
 %% Plotting
 %Plot the result of solver
@@ -34,42 +16,29 @@ tub_amp_plot = figure('Name','Tub_Amp_Plot', 'NumberTitle','off');
 figure(tub_amp_plot);
 tub_amp_plot.Position = [100, 250, 1250, 500];
 
-subplot(1, 2, 1)
+subplot(1, 3, 1)
 plot(t, y(:, 1))
-title("Disp (x)")
+title("Disp (t)")
 xlim([0, simDuration(2)])
 %ylim([-0.025, 0.025])
 
-subplot(1, 2, 2)
-%plot(t, y(:, 2))
-plot(time, out)
+subplot(1, 3, 2)
+plot(y(:, 3), y(:, 1))
+title("Disp(w)")
+xlim([0.6, 1.2])
+%ylim([0, round(omega_max/10)*10+10])
+
+subplot(1, 3, 3)
+plot(t, y(:, 3))
 title("Omega(t)")
-xlim([0, simDuration(2)])
-ylim([0, round(omega_max/10)*10+10])
+
 
 
 %% Functions
 
-function dydt = SHM(t, y, m, k, c, F0, omega_max, t_maxRPM)
-    omega = omega_F(t, omega_max, t_maxRPM);
-    evalin("base", "itr = itr + 1;");
-    
-    dydt = [y(2); F0/m*omega^2*0.25*sin(omega*t) -k/m*y(1) - c/m*y(2)];
+function dydt = SHM(t, y, m, k, c, F0, omega_rate)
+    dydt = zeros(3,1);
+    dydt(1) = y(2); 
+    dydt(2) = F0/m*sin(y(3)*t) -k/m*y(1) - c/m*y(2); 
+    dydt(3) = omega_rate;
 end 
-
-function omega = omega_F(t, omega_max, t_of_omega_max)
-    disp("t input to omega() = ")
-    disp(t)
-    if t < t_of_omega_max
-        omega = omega_max/t_of_omega_max*t;
-    else
-        %disp("Max omega reached");
-        omega = omega_max;
-    end
-%     hold on
-%     line = evalin("base", 'RPM_line');
-%     addpoints(line, t, omega)
-%     drawnow limitrate 
-    %plot(t, omega, '.', Color=red)
-    %pause(0.05)
-end
