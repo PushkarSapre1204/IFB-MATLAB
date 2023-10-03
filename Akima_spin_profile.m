50abaa56abb70f6017346c56cd3b4ba5ea32954f
PushkarSapre1204 <86977783+PushkarSapre1204@users.noreply.github.com>
2023-10-03 17:03:25 +0530

    Revert "Merge branch 'main' into Actual_spin_profile"
    This reverts commit 29ec5813ad9a7fa1ba0fe5870e5063a14b42baad, reversing
    changes made to fa3fe50f7e952c1340e4452c429423b555562bf3.

--- Akima_spin_profile.m
+++ Akima_spin_profile.m
@@ -1,8 +1,6 @@
-function rpm = Akima_spin_profile(spin_profile, method)    
-    t = 1:0.5:spin_profile(1, end);
-    figure("Name", "Spin profile", NumberTitle="off")
-    rpm = interp1(spin_profile(1,:), spin_profile(2,:), t, method);
-
+function rpm = Akima_spin_profile(spin_profile)    
+    t = 1:0.5:1130;
+    rpm = interp1(spin_profile(1,:), spin_profile(2,:), t, "makima");
     plot(t, rpm)
     hold on
     plot(spin_profile(1,:), spin_profile(2,:), '-o')
