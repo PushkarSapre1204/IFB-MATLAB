44b2315356a99c071a68c2f02b60c4aa533f918d
PushkarSapre1204 <86977783+PushkarSapre1204@users.noreply.github.com>
2023-10-03 12:47:01 +0530

    Omega variation as differential equation
    Here the omega is given as an additional equation to ODE45

--- ODE_test.m
+++ ODE_test.m
@@ -1,10 +1,11 @@
-k = 1;
-m = 1;
-c = 0.01 ;
-F0 = 1;
-omega_rate = 10^-5;
-simDuration = [0, 4*10^5];
-y0 = [0,0,0.6];
+k = 18;
+m = 60;
+c = 120;
+F0 = 0.5;
+simDuration = [0, 20];
+omega_max = 1400*2*pi/60;
+omega_rate = omega_max/simDuration(end);
+y0 = [0,0,0];
 
 % Solve the system. Store the amplitude and timesteps in a vector
 [t,y] = ode45(@(t,y) SHM(t, y, m, k, c, F0, omega_rate), simDuration, y0);
@@ -19,13 +20,13 @@ tub_amp_plot.Position = [100, 250, 1250, 500];
 subplot(1, 3, 1)
 plot(t, y(:, 1))
 title("Disp (t)")
-xlim([0, simDuration(2)])
+%xlim([0, simDuration(2)])
 %ylim([-0.025, 0.025])
 
 subplot(1, 3, 2)
 plot(y(:, 3), y(:, 1))
 title("Disp(w)")
-xlim([0.6, 1.2])
+%xlim([0.6, 1.2])
 %ylim([0, round(omega_max/10)*10+10])
 
 subplot(1, 3, 3)
@@ -39,6 +40,6 @@ title("Omega(t)")
 function dydt = SHM(t, y, m, k, c, F0, omega_rate)
     dydt = zeros(3,1);
     dydt(1) = y(2); 
-    dydt(2) = F0/m*sin(y(3)*t) -k/m*y(1) - c/m*y(2); 
+    dydt(2) = F0*y(3)^2*0.2/m*sin(y(3)*t) -k/m*y(1) - c/m*y(2); 
     dydt(3) = omega_rate;
 end 
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # MATLAB
  MATLAB simulations for IFB
 
+Getting correct/ okayish output. 
+peak at 114 RPM
+Note: FOrce not varying with time
