49fadfc32f0caabaf64694874b6fecebd269d129
PushkarSapre1204 <86977783+PushkarSapre1204@users.noreply.github.com>
2023-10-04 17:00:07 +0530

    Changed solver to ODE 89

--- ODE_45_wash.m
+++ ODE_45_wash.m
@@ -8,13 +8,13 @@ spinProfileIndex = 1;
 
 spinProfile = [0, 100, 120, 140, 160, 180, 200 ; 0, 300, 600, 900, 1200, 1400, 1400]
 
-load("SpinProfiles.mat")
-spinProfile = spinProfiles.split2;
+load("SpinProfile_linear.mat")
+%spinProfile = spinProfiles.split2;
 
 simDuration = spinProfile(1,end);
 
 
-[t, y]= ode45(@(t,y) tub_motion(t, y, m, k, c, m_unb, r, spinProfile), [0,simDuration], [0,0]);
+[t, y]= ode89(@(t,y) tub_motion(t, y, m, k, c, m_unb, r, spinProfile), [0,simDuration], [0,0]);
 
 %Create zero array for spin profile
 spin_profile_recreated = zeros(length(t), 1);
@@ -62,5 +62,5 @@ end
 
 function rpm = get_rpm(t, spin_profile)
     %build spin profile:
-    rpm = interp1(spin_profile(1,:), spin_profile(2,:), t, "pchip");
+    rpm = interp1(spin_profile(1,:), spin_profile(2,:), t, "linear");
 end
