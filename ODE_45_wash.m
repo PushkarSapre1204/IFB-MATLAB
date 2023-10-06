f279af60f782020109655667ee4dfb514b26f972
PushkarSapre1204 <86977783+PushkarSapre1204@users.noreply.github.com>
2023-10-06 12:28:50 +0530

    Set default spin profile to linear ramp
    Cleaned up code and added comments

--- ODE_45_wash.m
+++ ODE_45_wash.m
@@ -4,7 +4,7 @@ c = 120*2;
 m_unb = 0.5;
 r = 0.25;
 
-spinProfileIndex = 1;
+load("SpinProfile_linear.mat")
 
 spinProfile = [0, 100, 120, 140, 160, 180, 200 ; 0, 300, 600, 900, 1200, 1400, 1400]
 
@@ -21,26 +21,28 @@ spin_profile_recreated = zeros(length(t), 1);
 
 %Generate the spin profile using the time steps output of ODE45
 
+spin_profile_recreated = zeros(length(t), 1);                                                           %Create zero array for spin profile
+
 for i = 1:length(t)
-    spin_profile_recreated(i) = get_rpm(t(i), spinProfile);  
+    spin_profile_recreated(i) = get_rpm(t(i), spinProfile);                                             %Compute rpm corresponding to i'th time step
 end
 
-%% Plotting
+%% Plotting                                                  
+tub_amp_plot = figure('Name','Split 2: Pchip interp');                                                  %Create a new figure to display tub displacement plot                                                     
 
-%Create a new figure to display tub displacement plot
-tub_amp_plot = figure('Name','Split 2: Pchip interp');
+figure(tub_amp_plot)                                                                                    %Set figure as current figure
+tub_amp_plot.Position = [150, 250, 1250, 500];                                                          %Set figure popup location
 
-%Set figure as current figure
-figure(tub_amp_plot)
-tub_amp_plot.Position = [150, 250, 1250, 500];
-subplot(1, 2, 1)
+% Time vs Tub displacement 
+subplot(1, 2, 1)                              %Create subplot and set as the current figure    
 plot(t,y(:,1), 'blue');
 title("Tub displacement w.r.t time")
 xlabel("Time (s)")                                                                                                                                          
 ylabel("Tub displacement (m)")
-xlim([0,simDuration])
+xlim([0,simDuration])                         %Set X lim to ensure all data is visible                                              
 ylim([-0.005, 0.005])
 
+%Plot Time vs RPM. RPM is the regenerated graph
 subplot(1, 2, 2)
 plot(t,spin_profile_recreated, 'red');
 title("R.P.M w.r.t time")
@@ -53,6 +55,7 @@ ylim([0, 1500])
 %% Functions
 
 % Input to ODE45
+% System setup
 function dydt= tub_motion(t, y, m, k, c, m_unb, r, spin_profile)
     rpm = get_rpm(t, spin_profile);
     omega = rpm*2*pi/60;
