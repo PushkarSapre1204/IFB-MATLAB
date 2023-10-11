dd302135d383c3b4f016ebe14fe9a837fc3a8f26
PushkarSapre1204 <86977783+PushkarSapre1204@users.noreply.github.com>
2023-10-11 17:08:45 +0530

    Added main body
    Mainly get set methods are added. This are used for handling the dependent properties

--- +Attenuator/@Attenuator/Attenuator.m
+++ +Attenuator/@Attenuator/Attenuator.m
@@ -1,21 +1,77 @@
 classdef Attenuator 
-    properties  (SetAccess = immutable)
+    properties (Abstract, SetAccess = immutable)
         Constant
-        FixedNode = [] 
-        MobileNode = [1,2]
+    end
+    properties  (SetAccess = immutable, GetAccess = protected)
+        FixedNode = [0,0];       
+    end
+    properties  (GetAccess = protected)
+        MobileNode = [0,0];
+        MobileNodeVel = [0,0];
     end
     properties (Dependent)
-        FixedNodeX
-        FixedNodeY
+        FixedNodeX          % Xf
+        FixedNodeY          % Yf
+        MobileNodeX         % Xm
+        MobileNodeY         % Ym 
+        MobileNodeVelX      % Xm_dot
+        MobileNodeVelY      % Ym_dot
+    end
+    
+    %% Methods
+    
+    % Constructor
+    methods                 
+        function obj = Attenuator(Args)
+            arguments
+                Args.FNode = [0,0];
+                Args.MNode = [0,0];
+            end
+            obj.FixedNode = Args.FNode;
+            obj.MobileNode = Args.MNode;
+        end
     end
 
-    methods
+    % Get set methods
+    methods     
         function x = get.FixedNodeX(obj)
-            x = obj.FixedNodeX;
+            x = obj.FixedNode(1);
         end
         function y = get.FixedNodeY(obj)
-            y = obj.FixedNodeY;
+            y = obj.FixedNode(2);
         end
+
+        function x = get.MobileNodeX(obj)
+            x = obj.MobileNode(1);
+        end
+        function y = get.MobileNodeY(obj)
+            y = obj.MobileNode(2);
+        end
+
+        function x = get.MobileNodeVelX(obj)
+            x = obj.MobileNodeVel(1);
+        end
+        function y = get.MobileNodeVelY(obj)
+            y = obj.MobileNodeVel(2);
+        end
+        
+        function obj = set.MobileNode(obj, tubCenter)
+            obj.MobileNode = tubCenter + NodeOffset;
+        end
+
+        function obj = set.MobileNodeVelX(obj, VX)
+            obj.MobileNodeVelX = VX;
+        end
+        function obj = set.MobileNodeVelY(obj, VY)
+            obj.MobileNodeVelY = VY;
+        end
+        
     end
+
+    methods (Abstract)
+        f = Force(obj)
+    end
+
+
 end
 
