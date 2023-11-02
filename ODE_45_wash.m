364ff49559c32cb87d0300bc4079426bf88add9f
PushkarSapre1204 <86977783+PushkarSapre1204@users.noreply.github.com>
2023-11-02 16:01:33 +0530

    Made code inline with test script
    Tub center, velocity and force plotting added.
    Live tub location plotting added

--- ODE_45_wash.m
+++ ODE_45_wash.m
@@ -3,7 +3,7 @@ clear
 %% 
 Washer_Init
 load("SpinProfile_linear.mat");
-InitCond = [0, 0, 0, 0, 0];
+InitCond = [0.1, 0, 0, 0];
 
 %% Solver
 simDuration = spinProfile(1,end);                                                %Set sim duration
@@ -20,7 +20,26 @@ for i = 1:length(t)
     spin_profile_recreated(i) = get_rpm(t(i), spinProfile);                                             %Compute rpm corresponding to i'th time step
 end
 
-FDump = zeros(length(y), 4);
+% Make machine structure
+ReSolve = zeros(length(y), 4);
+
+LTub = figure("Name", "Live Tub");
+figure(LTub)
+xlim([-280*10^-3, 280*10^-3])
+ylim([-430*10^-3, 335*10^-3])
+hold on
+
+for i=1:length(Washer.Springs)
+    N = Washer.Springs{i,1}.getFixedNode();
+    plot(N(1), N(2), '-o', Color='red')     
+end
+
+for i=1:length(Washer.Dampers)
+    N = Washer.Dampers{i,1}.getFixedNode();
+    plot(N(1), N(2), '^', Color='blue')     
+end
+
+% Tub animation
 
 for i  = 1:length(t)
     % Update attenuators to next step 
@@ -28,14 +47,18 @@ for i  = 1:length(t)
     cellfun(@(Att) Att.Update(y(i, 1:2), y(i, 3:4)), Washer.Dampers); 
     SpringForce = sum(cell2mat(cellfun(@(Att) Att.Force(), Washer.Springs, 'UniformOutput', false)), 1);
     DamperForce = sum(cell2mat(cellfun(@(Att) Att.Force(), Washer.Dampers, 'UniformOutput', false)), 1);
-    FDump(i, 1:2) = SpringForce;
-    FDump(i, 3:4) = DamperForce;
+    ReSolve(i, 1:2) = SpringForce;
+    ReSolve(i, 3:4) = DamperForce;
+    plot(y(i, 1), y(i, 2), '.', Color='black')
+    pause(0.001)
 end
+
+
 %% Plotting                                                  
 %Plot the result of solver
 Output = figure('Name','Output', 'NumberTitle','off');
 figure(Output);
-Output.Position = [10, 375, 1500, 400];
+Output.Position = [10, 375, 1520, 400];
 
 subplot(1, 2, 1)
 plot(t, y(:, 1:2))
@@ -51,16 +74,18 @@ legend('Tub Vel X', 'Tub Vel Y')
 %xlim([0, simDuration(2)])
 %ylim([0, round(omega_max/10)*10+10])
 
+% Force plots
 ForceOut = figure("Name", "Force output", NumberTitle="off");
 figure(ForceOut)
+ForceOut.Position = [10, 50, 1500, 400];
 
 subplot(1,2,1)
-plot(t, FDump(:, 1:2))
+plot(t, ReSolve(:, 1:2))
 title(("Spring Force"))
 legend('Spring Fx', 'Spring Fy')
 
 subplot(1,2,2)
-plot(t, FDump(:, 3:4))
+plot(t, ReSolve(:, 3:4))
 title(("Damper Force"))
 legend('Damper Fx', 'Damper Fy')
 
