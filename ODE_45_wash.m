757df9535e64ad739ca0e709ca11e08ff99cd5f0
PushkarSapre1204 <86977783+PushkarSapre1204@users.noreply.github.com>
2023-11-04 10:48:26 +0530

    Gravity enabled
    Constant gravitational force added.

--- ODE_45_wash.m
+++ ODE_45_wash.m
@@ -2,8 +2,11 @@ clear
 
 %% 
 Washer_Init
-load("SpinProfile_linear.mat");
-InitCond = [0.1, 0, 0, 0];
+
+spinProfile = load("SpinProfile_linear.mat");
+spinProfile = spinProfile.spinProfile;
+
+InitCond = [0, 0, 0, 0, 0];
 
 %% Solver
 simDuration = spinProfile(1,end);                                                %Set sim duration
@@ -29,36 +32,51 @@ xlim([-280*10^-3, 280*10^-3])
 ylim([-430*10^-3, 335*10^-3])
 hold on
 
+% Display spring nodes
 for i=1:length(Washer.Springs)
     N = Washer.Springs{i,1}.getFixedNode();
     plot(N(1), N(2), '-o', Color='red')     
 end
 
+% Display damper nodes
 for i=1:length(Washer.Dampers)
     N = Washer.Dampers{i,1}.getFixedNode();
     plot(N(1), N(2), '^', Color='blue')     
 end
 
 % Tub animation
-
+%%
 for i  = 1:length(t)
     % Update attenuators to next step 
+    RPM = get_rpm(t(i), spinProfile);
+    Omega = 2*pi/60*RPM;
+    Theta = y(i, 5);
+
+    F_Unb = Washer.UnbMass * Washer.Radius.^2 * Omega;
+
     cellfun(@(Att) Att.Update(y(i, 1:2), y(i, 3:4)), Washer.Springs);
     cellfun(@(Att) Att.Update(y(i, 1:2), y(i, 3:4)), Washer.Dampers); 
     SpringForce = sum(cell2mat(cellfun(@(Att) Att.Force(), Washer.Springs, 'UniformOutput', false)), 1);
     DamperForce = sum(cell2mat(cellfun(@(Att) Att.Force(), Washer.Dampers, 'UniformOutput', false)), 1);
     ReSolve(i, 1:2) = SpringForce;
     ReSolve(i, 3:4) = DamperForce;
-    plot(y(i, 1), y(i, 2), '.', Color='black')
-    pause(0.001)
+
+    ReSolve(i, 5) = RPM; 
+
+    ReSolve(i, 6:7) = [F_Unb*cos(Theta), F_Unb*sin(Theta)];
+    %plot(y(i, 1), y(i, 2), '.', Color='black')
+    %pause(0.001)
 end
 
+plot(y(:, 1), y(:,2))
 
 %% Plotting                                                  
 %Plot the result of solver
+
+% Tub plots
 Output = figure('Name','Output', 'NumberTitle','off');
 figure(Output);
-Output.Position = [10, 375, 1520, 400];
+Output.Position = [10, 350, 1520, 400];
 
 subplot(1, 2, 1)
 plot(t, y(:, 1:2))
@@ -87,12 +105,28 @@ legend('Spring Fx', 'Spring Fy')
 subplot(1,2,2)
 plot(t, ReSolve(:, 3:4))
 title(("Damper Force"))
+
+
 legend('Damper Fx', 'Damper Fy')
+
+% RPM and Omega
+RPM_Dat = figure("Name", "RPM data");
+figure(RPM_Dat);
+ForceOut.Position = [10, 150, 1500, 400];
+
+subplot(1,2,1)
+plot(t, ReSolve(:, 5))
+title("RPM vs Time")
+
+subplot(1, 2, 2)
+plot(t, ReSolve(:, 6:7))
+title("Unbalance forces")
+legend("Force X", "Force Y")
 
 %% Functions
 
 function rpm = get_rpm(t, spin_profile)
     %build spin profile:
-    rpm = 1400;
-    %rpm = interp1(spin_profile(1,:), spin_profile(2,:), t, "pchip");
+    %rpm = 1400;
+    rpm = interp1(spin_profile(1,:), spin_profile(2,:), t, "pchip");
 end
--- Washer_Init.m
+++ Washer_Init.m
@@ -3,11 +3,12 @@ import Attenuator.*
 Washer.Mass = 60;
 Washer.UnbMass = 0.5;
 Washer.Radius = 0.3;
+Washer.Fg = [0, -9.81*Washer.Mass];
 TC = [0,0];
 
 %% Actual Washer
-Washer.Springs(1,1) = {Spring("FNode", [-275.3*10^-3, 332.33*10^-3], "MNode", [-253.5*10^-3, 130*10^-3], "tubCenter", [0,0], "K", 9, "L0", 172*10^-3)};
-Washer.Springs(2,1) = {Spring("FNode", [ 275.3*10^-3, 332.33*10^-3], "MNode", [ 253.5*10^-3, 130*10^-3], "tubCenter", [0,0], "K", 9, "L0", 172*10^-3)};
+Washer.Springs(1,1) = {Spring("FNode", [-275.3*10^-3, 332.33*10^-3], "MNode", [-253.5*10^-3, 130*10^-3], "tubCenter", [0,0], "K", 9000, "L0", 172*10^-3)};
+Washer.Springs(2,1) = {Spring("FNode", [ 275.3*10^-3, 332.33*10^-3], "MNode", [ 253.5*10^-3, 130*10^-3], "tubCenter", [0,0], "K", 9000, "L0", 172*10^-3)};
 
 Washer.Dampers(1,1) = {Damper("FNode", [-263.20*10^-3, -422.6*10^-3], "MNode", [-186.33*10^-3, -218.22*10^-3], "tubCenter", [0,0], "C", 120)};
 Washer.Dampers(2,1) = {Damper("FNode", [ 263.20*10^-3, -422.6*10^-3], "MNode", [ 186.33*10^-3, -218.22*10^-3], "tubCenter", [0,0], "C", 120)};
