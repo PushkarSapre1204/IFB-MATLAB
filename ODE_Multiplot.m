f286cd1bb622871e3ffa8f2b3618d8ea303ec1dc
PushkarSapre1204 <86977783+PushkarSapre1204@users.noreply.github.com>
2023-11-07 17:33:29 +0530

    Added script to solve multiple conditions
    Multiplot solves each case parallelly and gives output compatible with PostPlot

--- ODE_Multiplot.m
+++ ODE_Multiplot.m
@@ -1,21 +1,29 @@
 function ODE_Multiplot
-    Washer_Init
-    
-    spinProfile = load("Actual_spin_profile.mat");
-    spinProfile = spinProfile.spinProfile;
-    
-    InitCond = [0, 0, 0, 0, 0];
-    
-    %% Solver
-    simDuration = spinProfile(1,end);
-    
-    [t, y]= ode45(@(t,y) Washer_2DOF(t, y, Washer, spinProfile), [0,simDuration], InitCond);
-    
-    %%
-    
-    Results.RPM_800 = PostProc(t,y(:, 5), Washer, 100);
-    Results.RPM_1000
-    Results.RPM_1200
-    Results.RPM_1400
-    Results.RPM_1600
+clear
+%% Setup
+Washer_Init                                             % Initialise washer
+S = load("SpinProfiles.mat");                           % Load the required spin profiles
+                                                          
+InitCond = [0, 0, 0, 0, 0];
+
+ProfileSet = S.spinProfiles.Constant;
+SpinProfs = struct2cell(ProfileSet);         % Convert input struc to cell array for indexing
+
+%FieldNames = ["RPM_0800", "RPM_1000", "RPM_1200", "RPM_1400"];
+
+%% Solver
+SolOut = {0, 0, 0, 0};                                 % Initialise zero array to store results
+PostOut = {0, 0, 0, 0};
+
+parfor i = 1:length(SpinProfs)
+    simDuration = 200;                     
+    disp(['Solving set', num2str(i)])
+    [t, y]= ode45(@(t,y) Washer_2DOF(t, y, Washer, SpinProfs{i}), [0,simDuration], InitCond);
+    disp(['Solve ', num2str(i), ' complete'])
+    SolOut{i} = [t,y];
+    disp(['Processing solve data..', num2str(i)])
+    PostOut{i} = PostProc(t, y, Washer, SpinProfs{i});
+end
+%%
+   
 end
