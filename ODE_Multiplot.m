f1bf0e64b70a4ae0e807971e16666309b1b3d110
PushkarSapre1204 <86977783+PushkarSapre1204@users.noreply.github.com>
2023-12-07 15:08:06 +0530

    Added switch case to Multiplot
    Different washer setups can now be tested via multiplot

--- ODE_Multiplot.m
+++ ODE_Multiplot.m
@@ -1,12 +1,11 @@
 clear
 %% Setup
-Washer = Washer_Init("Actual");                                             % Initialise washer
+VarPar = "Washer";
+%VarPar = "SpinProf";
+
 S = load("SpinProfiles.mat");                           % Load the required spin profiles
                                                           
 InitCond = [0, 0, 0, 0, 0];
-
-ProfileSet = S.spinProfiles.Constant;
-SpinProfs = struct2cell(ProfileSet);         % Convert input struc to cell array for indexing
 
 %FieldNames = ["RPM_0800", "RPM_1000", "RPM_1200", "RPM_1400"];
 
@@ -14,15 +13,43 @@ SpinProfs = struct2cell(ProfileSet);         % Convert input struc to cell array
 SolOut = {0, 0, 0, 0};                                 % Initialise zero array to store results
 PostOut = {0, 0, 0, 0};
 
-parfor i = 1:length(SpinProfs)
-    simDuration = 200;                     
-    disp(['Solving set ', num2str(i)])
-    [t, y]= ode45(@(t,y) Washer_2DOF(t, y, Washer, SpinProfs{i}), [0,simDuration], InitCond);
-    disp(['Solve ', num2str(i), ' complete'])
-    SolOut{i} = [t,y];
-    disp(['Processing solve data..', num2str(i)])
-    PostOut{i} = PostProc(t, y, Washer, SpinProfs{i});
-    disp(["Processing ", i, " complete"])
+switch VarPar
+    case "Washer"
+        %Washers = zeros{1, 20};
+    
+        for i = 1:20
+            Washers(i) = Washer_Init("Custom", "SStiff", i);
+        end
+        SpinProfs = S.spinProfiles.Actual.Base;
+
+        simDuration = SpinProfs(1,end);
+        
+        parfor i = 1:length(Washers)
+            disp(['Solving set ', num2str(i)])
+            [t, y]= ode45(@(t,y) Washer_2DOF(t, y, Washers(i), SpinProfs), [0,simDuration], InitCond);
+            disp(['Solve ', num2str(i), ' complete'])
+            SolOut{i} = [t,y];
+            disp(['Processing solve data..', num2str(i)])
+            PostOut{i} = PostProc(t, y, Washers(i), SpinProfs);
+            disp(["Processing ", i, " complete"])
+        end
+
+    case "SpinProf"
+        ProfileSet = S.spinProfiles.Constant;
+        SpinProfs = struct2cell(ProfileSet);         % Convert input struc to cell array for indexing
+        Washer = Washer_Init("Existing");            % Initialise washer
+
+        parfor i = 1:length(SpinProfs)
+            simDuration = 200;
+            disp(['Solving set ', num2str(i)])
+            [t, y]= ode45(@(t,y) Washer_2DOF(t, y, Washer, SpinProfs{i}), [0,simDuration], InitCond);
+            disp(['Solve ', num2str(i), ' complete'])
+            SolOut{i} = [t,y];
+            disp(['Processing solve data..', num2str(i)])
+            PostOut{i} = PostProc(t, y, Washer, SpinProfs{i});
+            disp(["Processing ", i, " complete"])
+        end
+
 end
 disp("Plotting data")
 %%
