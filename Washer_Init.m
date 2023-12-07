c5b4e32a40a81f8f1b4f28da8dd56eab04aed231
PushkarSapre1204 <86977783+PushkarSapre1204@users.noreply.github.com>
2023-12-07 15:11:14 +0530

    Added help section
    Added help section to for future uses

--- Washer_Init.m
+++ Washer_Init.m
@@ -1,12 +1,19 @@
 function Washer = Washer_Init(Mode, Args)
+% Function used to setup the washer system
+% Modes: Existing: Setup pararmters of existing 9Kg MX washer used
+% Custom: To generate washer with custom parameters
+% 2-DOF test: Generates a 2 DOF spring damper setup to test new solve algorithms. It uses unit values for easier debugging. 
+% 1-DOF test: Generates a 1 DOF (Linear) spring damper setup to test new solvers. 
+
+
 arguments
     Mode
-    Args.SFNode
-    Args.DFNode
-    Args.SMNode
-    Args.DMNode
-    Args.SStiff
-    Args.DStiff
+    Args.SFNode = [-275.30*10^-3, 332.33*10^-3]
+    Args.DFNode = [-263.20*10^-3, -422.6*10^-3]
+    Args.SMNode = [-253.5*10^-3, 130*10^-3]
+    Args.DMNode = [-186.33*10^-3, -218.22*10^-3]
+    Args.SStiff = 9000
+    Args.DStiff = 120
 end
 
 import Attenuator.*
@@ -28,10 +35,10 @@ switch Mode
     
     case "Custom"
         Washer.Springs(1,1) = {Spring("FNode", Args.SFNode, "MNode", Args.SMNode, "tubCenter", TC, "K", Args.SStiff, "L0", 172*10^-3)};
-        Washer.Springs(2,1) = {Spring("FNode", Args.SFNode, "MNode", Args.SMNode, "tubCenter", TC, "K", Args.SStiff, "L0", 172*10^-3)};
+        Washer.Springs(2,1) = {Spring("FNode", Args.SFNode.*[-1,1], "MNode", Args.SMNode.*[-1,1], "tubCenter", TC, "K", Args.SStiff, "L0", 172*10^-3)};
 
         Washer.Dampers(1,1) = {Damper("FNode", Args.DFNode, "MNode", Args.DMNode, "tubCenter", TC, "C", Args.DStiff)};
-        Washer.Dampers(2,1) = {Damper("FNode", Args.DFNode, "MNode", Args.DMNode, "tubCenter", TC, "C", Args.DStiff)};
+        Washer.Dampers(2,1) = {Damper("FNode", Args.DFNode.*[-1,1], "MNode", Args.DMNode.*[-1,1], "tubCenter", TC, "C", Args.DStiff)};
 
 
 
@@ -54,6 +61,7 @@ switch Mode
 end
 clear TC
 
+% Tub structure for 3-DOF
 Washer.Tub = transpose([Washer.Springs{1,1}.getNodeOffset; Washer.Springs{2,1}.getNodeOffset; Washer.Dampers{1,1}.getNodeOffset; Washer.Dampers{2,1}.getNodeOffset]);
 end
 
