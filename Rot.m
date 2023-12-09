2268fdd1a049b9a9951826c439842768a79b16da
PushkarSapre1204 <86977783+PushkarSapre1204@users.noreply.github.com>
2023-12-09 10:57:56 +0530

    Added placeholders for X,Y rotations

--- Rot.m
+++ Rot.m
@@ -6,12 +6,12 @@ function R = Rot(Axis, Ang, Unit)
         switch Axis
     
             case 'Z'
-                R = [cos(Ang), -sin(Ang); sin(Ang), cos(Ang)];
+                R = [cos(Ang), -sin(Ang), 0; sin(Ang), cos(Ang), 0; 0, 0, 1];
            
             case 'Y'
-            
+                disp("Update pending")
             case 'X'
-            
+                disp("Update pending")
             otherwise
                 disp("Incorrect rotation axis")
         end
