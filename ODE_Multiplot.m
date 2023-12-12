10f4317e448bda4abdd42022d9e8b3348f390d36
PushkarSapre1204 <86977783+PushkarSapre1204@users.noreply.github.com>
2023-12-12 09:19:40 +0530

    Fixed indexing issue
    Washer is a cell array and requires {} indexing. Changed all relevant indexing from () to {}

--- ODE_Multiplot.m
+++ ODE_Multiplot.m
@@ -4,32 +4,37 @@ VarPar = "Washer";
 %VarPar = "SpinProf";
 
 S = load("SpinProfiles.mat");                           % Load the required spin profiles
-                                                          
+
 %FieldNames = ["RPM_0800", "RPM_1000", "RPM_1200", "RPM_1400"];
+
 
 %% Solver
 switch VarPar
     case "Washer"
-        SolOut = zeros(1, 20);                                 % Initialise zero array to store results
-        PostOut = zeros(1, 20);
-        Washers = zeros(1, 20);
-        Legend = zeros(1,20);
+
         % Create varying washer setups with parameters as required
         for i = 1:20
-            Washers(i) = Washer_Init("Custom", "SStiff", i*1000);
+            Washers{i} = Washer_Init("Custom", "SStiff", i*1000);
+            Legend(i) = "SStiff " + i
         end
         
         % Load the normal spinprofile and set the simulation duration
         SpinProfs = S.spinProfiles.Actual.Base;
         simDuration = SpinProfs(1,end);
+        InitCond = [0,0,0,0,0];
+
+        %SolOut = zeros(1, 20);          % Initialise zero array to store results
+        %PostOut = zeros(1, 20);
+        % Washers = zeros(1, 20);
+        Legend = zeros(1,20);
         
         parfor i = 1:length(Washers)
             disp(['Solving set ', num2str(i)])
-            [t, y]= ode45(@(t,y) Washer_2DOF(t, y, Washers(i), SpinProfs), [0,simDuration], InitCond);
+            [t, y]= ode45(@(t,y) Washer_2DOF(t, y, Washers{i} , SpinProfs), [0,simDuration], InitCond);
             disp(['Solve ', num2str(i), ' complete'])
             SolOut{i} = [t,y];
             disp(['Processing solve data..', num2str(i)])
-            PostOut{i} = PostProc(t, y, Washers(i), SpinProfs);
+            PostOut{i} = PostProc(t, y, Washers{i}, SpinProfs);
             disp(["Processing ", i, " complete"])
             Legend(i) = "SStiff " + i;
         end
